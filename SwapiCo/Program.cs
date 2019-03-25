c65cf4c9bbdcb8fd1e564af26355d88c99eb85c2
Columbia91 <alpha.z91@mail.ru>
2019-03-25 21:41:12 +0600

    Добавьте файлы проекта.

---
+++ SwapiCo/Program.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapiCo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string json;
+            string path = $"https://swapi.co/api/people/10";
+
+
+
+            using (WebClient client = new WebClient())
+            {
+                json = client.DownloadString(path);
+            }
+
+            var characters = JsonConvert.DeserializeObject<StarWarsCharacters>(json);
+
+            Console.WriteLine(json);
+
+
+        }
+    }
+}
---
+++ SwapiCo/StarWarsCharacters.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace SwapiCo
+{
+    [Serializable]
+    class StarWarsCharacters
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("height")]
+        public string Height { get; set; }
+
+        [JsonProperty("mass")]
+        public string Mass { get; set; }
+
+        [JsonProperty("hair_color")]
+        public string Hair_color { get; set; }
+
+        [JsonProperty("skin_color")]
+        public string Skin_color { get; set; }
+
+        [JsonProperty("eye_color")]
+        public string Eye_color { get; set; }
+
+        [JsonProperty("birth_year")]
+        public string Birth_year { get; set; }
+
+        [JsonProperty("gender")]
+        public string Gender { get; set; }
+
+        [JsonProperty("homeworld")]
+        public string Homeworld { get; set; }
+
+        [JsonProperty("films")]
+        public IList<string> Films { get; set; }
+
+        [JsonProperty("species")]
+        public IList<string> Species { get; set; }
+
+        [JsonProperty("vehicles")]
+        public IList<string> Vehicles { get; set; }
+
+        [JsonProperty("starships")]
+        public IList<string> Starships { get; set; }
+
+        [JsonProperty("created")]
+        public DateTime Created { get; set; }
+
+        [JsonProperty("edited")]
+        public DateTime Edited { get; set; }
+
+        [JsonProperty("url")]
+        public string Url { get; set; }
+
+        public void Show()
+        {
+            Console.WriteLine("name: " + Name +
+                "\nheight: " + Species +
+                "\nmass: " + Gender +
+                "\nhair_color: " + House +
+                "\nskin_color: " + DateOfBirth +
+                "\neye_color: " + YearOfBirth +
+                "\nbirth_year: " + Ancestry +
+                "\ngender: " + EyeColour +
+                "\nHairColour: " + HairColour +
+                "\nWood: " + Wand.Wood +
+                "\nCore: " + Wand.Core +
+                "\nLength: " + Wand.Length +
+                "\nPatronus: " + Patronus +
+                "\nHogwartsStudent: " + HogwartsStudent +
+                "\nHogwartsStaff: " + HogwartsStaff +
+                "\nActor: " + Actor +
+                "\nAlive: " + Alive +
+                "\nImage: " + Image + "\n\n");
+        }
+    }
+}
